a012bf922aba3c18377c83e64b8babd97bfaa1cc
Paul Higgins <paul.higgins@mozo.com.au>
2012-08-11 20:59:30 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PcycMobilev2App::Application.config.secret_token = '9e77ffe39623a901fdc872173355c5d6ddd8ebecbd555f986d2607fd3616de6aa4cd7ac06e593476a46e355409ab7615d531854af1f7e7d2ef646583b772965e'
